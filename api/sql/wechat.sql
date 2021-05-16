eb83c94e338be1edd2d31bec2e94aaab236c835d
metanoia1989 <sogaxili@gmail.com>
2021-05-16 22:23:34 +0800

    feat: 完成微信相关数据表的设计

--- api/sql/wechat.sql
+++ api/sql/wechat.sql
@@ -40,7 +40,7 @@ COMMENT='微信群组表';
 
 
 -- 联系人 群组关联表    
-CREATE TABLE `ts_wechat_room` (
+CREATE TABLE `ts_wechat_room_contact` (
     `room_ident` VARCHAR(255) NOT NULL COMMENT '群组',
     `contact_ident` VARCHAR(255) NOT NULL COMMENT '联系人标识ID',
     UNIQUE INDEX `relative` (`room_ident`, `contact_ident`)
@@ -48,3 +48,43 @@ CREATE TABLE `ts_wechat_room` (
 ENGINE=InnoDB
 COLLATE='utf8_general_ci'
 COMMENT='微信群组联系人关联表';
+
+
+--- 消息类型表 数量不多，直接写死到源码里
+-- export enum MessageType {
+--   Unknown = 0,
+
+--   Attachment  = 1,    // Attach(6), 附件
+--   Audio       = 2,    // Audio(1), Voice(34) 音频 语音
+--   Contact     = 3,    // ShareCard(42) 名片卡
+--   ChatHistory = 4,    // ChatHistory(19) 消息历史
+--   Emoticon    = 5,    // Sticker: Emoticon(15), Emoticon(47)  表情
+--   Image       = 6,    // Img(2), Image(3)  图片
+--   Text        = 7,    // Text(1) 文本
+--   Location    = 8,    // Location(48) 定位
+--   MiniProgram = 9,    // MiniProgram(33) 小程序
+--   GroupNote   = 10,   // GroupNote(53) 群通知
+--   Transfer    = 11,   // Transfers(2000) 
+--   RedEnvelope = 12,   // RedEnvelopes(2001)
+--   Recalled    = 13,   // Recalled(10002) 撤销
+--   Url         = 14,   // Url(5) URL
+--   Video       = 15,   // Video(4), Video(43) 视频消息
+-- }
+
+--- 消息表
+CREATE TABLE `ts_wechat_message` (
+    `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `msg_ident` VARCHAR(255) NOT NULL COMMENT '消息标识ID',
+    `filename` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '文件名', 
+    `fromId` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '消息发送人ID', 
+    `roomId` VARCHAR(255) COMMENT '所在群组ID，好友消息此字段为空', 
+    `toId` VARCHAR(255)  COMMENT '消息发送的目标，如果为群组消息，则目标为群主ID', 
+    `content` TEXT COMMENT '消息内容',
+    `type` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '消息类型 0 未知 6 图片 7 文本',
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '消息发送时间',
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `msg_ident` (`msg_ident`) 
+)
+ENGINE=MyISAM
+COLLATE='utf8_general_ci'
+COMMENT='微信群组表';
