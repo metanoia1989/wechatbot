d3d3eb29bb1fb205982dd0d438318b2c150d51be
metanoia1989 <sogaxili@gmail.com>
2021-05-30 09:31:35 +0800

    feat: 添加进程重启

--- api/index.js
+++ api/index.js
@@ -10,11 +10,19 @@ const db = new DB().getInstance()
 const Bot = require('./bot')
 const start_app_server = require('./api')
 
+const restartProcess = () => {
+    spawn(process.argv[1], process.argv.slice(2), {
+      detached: true, 
+      stdio: ['ignore', out, err]
+    }).unref()
+    process.exit()
+}
+
 // 处理未捕获的Promise异常，一般是wechaty库的原因
 process.on('unhandledRejection', error => {
     console.error('unhandledRejection', error);
-    delete Bot.getInstance()
-    Bot.getInstance()
+    console.error("出现异常了，进行进程重启，后面需要单独写个进程来接收消息")
+    restartProcess()
 });
 
 Bot.getInstance()
